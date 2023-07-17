e79eeb4ed3344e7ff896e0c1f12f5e1aac2dc773
oliverduce@googlemail.com <oliverduce@googlemail.com>
2023-07-17 06:06:38 +0000

    Created ServerModule1 and Contact; Edited theme.css, settings and theme/parameters.yaml; Deleted Form1

---
+++ client_code/Contact/__init__.py
@@ -0,0 +1,23 @@
+from ._anvil_designer import ContactTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Contact(ContactTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_submit_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name = self.text_box_name.text
+    email = self.text_box_email.text
+    feedback = self.text_area_feedback.text
+    submit = anvil.server.call('add_feedback', name, email, feedback)
+    alert("Feedback received. Thanks!")
+    return submit
+
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,15 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+from datetime import datetime
+
+@anvil.server.callable
+def add_feedback(name,email,feedback):
+  app_tables.feedback.add_row(
+  name= name,
+  email= email,
+  feedback = feedback,
+  created = datetime.now()
+  )
+ 
